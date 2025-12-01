9866a7d0ed45534f8c9955cc8b3c9e379636cd28
TuTech <tulay@tutechit.com>
2025-11-30 20:12:23 -0500

    Created CheckGroupingSymbols.java
    This program:
    
        - Uses a Stack
        - eads the file from command-line argument
        - Checks ( ), { }, and [ ]
        - Ignores characters that are not grouping symbols
        - Detects mismatched symbols and overlapping pairs
        - Reports whether the file is balanced or not

---
+++ CheckGroupingSymbols.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+    This program:
+
+    - Uses a Stack
+    - eads the file from command-line argument
+    - Checks ( ), { }, and [ ]
+    - Ignores characters that are not grouping symbols
+    - Detects mismatched symbols and overlapping pairs
+    - Reports whether the file is balanced or not
+ */
+public class CheckGroupingSymbols {
+    
+}
